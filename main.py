f99e558614168c74472d4019a2ce9798b1687790
Kate <kittysht@gmail.com>
2022-11-25 17:26:44 +0300

    commit 2

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import sys
 import random
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLineEdit
+from UI import Ui_MainWindow
 
 
-class Okr(QMainWindow):
+class Okr(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.setWindowTitle('Git и желтые окружности')
+        self.setWindowTitle('Git и случайные окружности')
         self.pushButton.clicked.connect(self.yell)
 
     def yell(self):
@@ -19,7 +20,7 @@ class Okr(QMainWindow):
         self.do_paint = True
         qp = QPainter()
         qp.begin(self)
-        c = QColor(255,255,0)
+        c = QColor(random.randint(0, 255),random.randint(0, 255),random.randint(0, 255))
         qp.setBrush(c)
         s = random.randint(16, 100)
         qp.drawEllipse(random.randint(0, self.size().width()) - (s // 2), random.randint(0, self.size().height()) - (s // 2), s, s)
